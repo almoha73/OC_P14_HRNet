37cf4d0bb2942f82488af4f21e6f52c816c3b523
Agnes <almoha@gmail.com>
2022-11-27 14:34:29 +0100

    navigation ok / reste mise en page de la page tableau

--- src/pages/CreateEmployee.js
+++ src/pages/CreateEmployee.js
@@ -1,10 +1,329 @@
 import React from "react";
+import { Fragment, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import logo from "../assets/logo.jpg";
+import { Link } from "react-router-dom";
+
+const people = [
+	{ id: 1, name: "Wade Cooper" },
+	{ id: 2, name: "Arlene Mccoy" },
+	{ id: 3, name: "Devon Webb" },
+	{ id: 4, name: "Tom Cook" },
+	{ id: 5, name: "Tanya Fox" },
+	{ id: 6, name: "Hellen Schmidt" },
+	{ id: 7, name: "Caroline Schultz" },
+	{ id: 8, name: "Mason Heaney" },
+	{ id: 9, name: "Claudie Smitham" },
+	{ id: 10, name: "Emil Schaefer" },
+];
+const department = [
+	{ id: 1, name: "Sales" },
+	{ id: 2, name: "Marketing" },
+	{ id: 3, name: "Engeneering" },
+	{ id: 4, name: "Human Resources" },
+	{ id: 5, name: "Legal" },
+];
+function classNames(...classes) {
+	return classes.filter(Boolean).join(" ");
+}
 
 const CreateEmployee = () => {
+	const [selected, setSelected] = useState(people[3]);
+	const [selectedDpt, setSelectedDpt] = useState(department[3]);
 	return (
-		<div>
-			<h1 className="text-6xl">Create Employee</h1>
-		</div>
+		<>
+			<div className="title flex items-center justify-between  w-1/2 mx-auto mt-12 ">
+				<img className="w-40" src={logo} alt="" />
+				<h1 className="text-center text-7xl text-green-700">HRNet</h1>
+				<Link
+					to="/employee-list"
+					className="inline-flex items-center rounded-md border border-transparent bg-green-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+				>
+					View Current Employees
+				</Link>
+			</div>
+			<h2 className="text-center text-5xl text-green-700 mt-12">
+				Create Employee
+			</h2>
+
+			<form className="w-3/5 sm:w-2/5 mx-auto mt-8">
+				<div>
+					<label
+						htmlFor="firstname"
+						className="block text-sm font-medium text-gray-700"
+					>
+						Firstname
+					</label>
+					<div className="mt-1">
+						<input
+							type="text"
+							name="firstname"
+							id="firstname"
+							className="block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
+							placeholder="Firstname"
+						/>
+					</div>
+				</div>
+				<div className="mt-8">
+					<label
+						htmlFor="lastname"
+						className="block text-sm font-medium text-gray-700"
+					>
+						Lastname
+					</label>
+					<div className="mt-1">
+						<input
+							type="text"
+							name="lastname"
+							id="lastname"
+							className="block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
+							placeholder="Lastname"
+						/>
+					</div>
+				</div>
+				<div className="mt-8">
+					<label
+						htmlFor="birthDate"
+						className="block text-sm font-medium text-gray-700"
+					>
+						Date of Birth
+					</label>
+					<div className="mt-1">
+						<input
+							type="date"
+							name="birthDate"
+							id="birthDate"
+							className="block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
+							placeholder="birthDate"
+						/>
+					</div>
+				</div>
+				<div className="mt-8">
+					<label
+						htmlFor="startdate"
+						className="block text-sm font-medium text-gray-700"
+					>
+						Start Date
+					</label>
+					<div className="mt-1">
+						<input
+							type="date"
+							name="startdate"
+							id="startdate"
+							className="block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
+							placeholder="startdate"
+						/>
+					</div>
+				</div>
+				<div className="adress border mt-8 bg-green-200">
+					<h1 className="text-green-600 text-center mt-8">Address</h1>
+					<div className="mt-8 w-1/2 mx-auto">
+						<label
+							htmlFor="street"
+							className="block text-sm font-medium text-gray-700"
+						>
+							Street
+						</label>
+						<div className="mt-1">
+							<input
+								type="text"
+								name="street"
+								id="street"
+								className="block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
+								placeholder="street"
+							/>
+						</div>
+					</div>
+					<div className="mt-8 w-1/2 mx-auto">
+						<label
+							htmlFor="city"
+							className="block text-sm font-medium text-gray-700"
+						>
+							City
+						</label>
+						<div className="mt-1">
+							<input
+								type="text"
+								name="city"
+								id="city"
+								className="block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
+								placeholder="city"
+							/>
+						</div>
+					</div>
+					<Listbox value={selected} onChange={setSelected}>
+						{({ open }) => (
+							<div className="w-1/2 mx-auto">
+								<Listbox.Label className="block text-sm font-medium text-gray-700 mt-8">
+									State
+								</Listbox.Label>
+								<div className="relative mt-1">
+									<Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-green-500 focus:outline-none focus:ring-1 focus:ring-green-500 sm:text-sm">
+										<span className="block truncate">{selected.name}</span>
+										<span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+											<ChevronUpDownIcon
+												className="h-5 w-5 text-gray-400"
+												aria-hidden="true"
+											/>
+										</span>
+									</Listbox.Button>
+
+									<Transition
+										show={open}
+										as={Fragment}
+										leave="transition ease-in duration-100"
+										leaveFrom="opacity-100"
+										leaveTo="opacity-0"
+									>
+										<Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+											{people.map((person) => (
+												<Listbox.Option
+													key={person.id}
+													className={({ active }) =>
+														classNames(
+															active
+																? "text-white bg-green-600"
+																: "text-gray-900",
+															"relative cursor-default select-none py-2 pl-8 pr-4"
+														)
+													}
+													value={person}
+												>
+													{({ selected, active }) => (
+														<>
+															<span
+																className={classNames(
+																	selected ? "font-semibold" : "font-normal",
+																	"block truncate"
+																)}
+															>
+																{person.name}
+															</span>
+
+															{selected ? (
+																<span
+																	className={classNames(
+																		active ? "text-white" : "text-green-600",
+																		"absolute inset-y-0 left-0 flex items-center pl-1.5"
+																	)}
+																>
+																	<CheckIcon
+																		className="h-5 w-5"
+																		aria-hidden="true"
+																	/>
+																</span>
+															) : null}
+														</>
+													)}
+												</Listbox.Option>
+											))}
+										</Listbox.Options>
+									</Transition>
+								</div>
+							</div>
+						)}
+					</Listbox>
+					<div className="mt-8 w-1/2 mx-auto mb-8">
+						<label
+							htmlFor="zip"
+							className="block text-sm font-medium text-gray-700"
+						>
+							Zip Code
+						</label>
+						<div className="mt-1">
+							<input
+								type="number"
+								name="zip"
+								id="zip"
+								className="block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
+								placeholder="zip"
+							/>
+						</div>
+					</div>
+				</div>
+				<Listbox value={selectedDpt} onChange={setSelectedDpt}>
+					{({ open }) => (
+						<div className="w-full mt-8">
+							<Listbox.Label className="block text-sm font-medium text-gray-700">
+								Department
+							</Listbox.Label>
+							<div className="relative mt-1">
+								<Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-green-500 focus:outline-none focus:ring-1 focus:ring-green-500 sm:text-sm">
+									<span className="block truncate">{selectedDpt.name}</span>
+									<span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+										<ChevronUpDownIcon
+											className="h-5 w-5 text-gray-400"
+											aria-hidden="true"
+										/>
+									</span>
+								</Listbox.Button>
+
+								<Transition
+									show={open}
+									as={Fragment}
+									leave="transition ease-in duration-100"
+									leaveFrom="opacity-100"
+									leaveTo="opacity-0"
+								>
+									<Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+										{department.map((elt) => (
+											<Listbox.Option
+												key={elt.id}
+												className={({ active }) =>
+													classNames(
+														active
+															? "text-white bg-green-600"
+															: "text-gray-900",
+														"relative cursor-default select-none py-2 pl-3 pr-9"
+													)
+												}
+												value={department}
+											>
+												{({ selectedDpt, active }) => (
+													<>
+														<span
+															className={classNames(
+																selectedDpt ? "font-semibold" : "font-normal",
+																"block truncate"
+															)}
+														>
+															{elt.name}
+														</span>
+
+														{selectedDpt ? (
+															<span
+																className={classNames(
+																	active ? "text-white" : "text-green-600",
+																	"absolute inset-y-0 right-0 flex items-center pr-4"
+																)}
+															>
+																<CheckIcon
+																	className="h-5 w-5"
+																	aria-hidden="true"
+																/>
+															</span>
+														) : null}
+													</>
+												)}
+											</Listbox.Option>
+										))}
+									</Listbox.Options>
+								</Transition>
+							</div>
+						</div>
+					)}
+				</Listbox>
+				<div className="w-full flex justify-center mt-8 mb-8">
+					<button
+						type="button"
+						className="inline-flex items-center rounded-md border border-transparent bg-green-100 px-6 py-3 text-base font-medium text-green-700 hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+					>
+						Save
+					</button>
+				</div>
+			</form>
+		</>
 	);
 };
 
--- src/pages/EmployeeList.js
+++ src/pages/EmployeeList.js
@@ -1,10 +1,276 @@
 import React from "react";
+import logo from "../assets/logo.jpg";
+import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import { Link } from "react-router-dom";
+
+const people = [
+	{
+		Firstname: "Agnès",
+		Lastname: "Beaumatin",
+		StartDate: "10/18/2021",
+		Department: "Dev Web",
+		BirthDate: "10/18/1973",
+		Street: "38 rue Commandant Hautreux",
+		City: "Bordeaux",
+		State: "AZ",
+		Zipcode: "33300",
+	},
+	// More people...
+];
 
 const EmployeeList = () => {
 	return (
-		<div>
-			<h1 className="text-6xl">Liste employees</h1>
-		</div>
+		<>
+			<img className="mx-auto mt-8" src={logo} alt="logo" />
+			<h1 className="text-center text-5xl sm:mt-8 sm:mb-16 text-green-700 font-bold">
+				Current Employees
+			</h1>
+			<div className="px-4 sm:px-6 lg:px-8 ">
+				<div className="sm:flex sm:items-center sm:mb-12 ">
+					<div className="sm:flex sm:flex-col sm:w-1/4 mb-4">
+						<h1 className="text-xl font-semibold text-gray-900">Employees</h1>
+						<p className="mt-2 text-sm text-gray-700">
+							A list of all the HRnet employees !
+						</p>
+					</div>
+					<div className="flex sm:w-1/4 sm:justify-center">
+						<label
+							htmlFor="numb"
+							className="block text-sm font-medium text-gray-700 mt-3"
+						>
+							Show
+						</label>
+						<select
+							id="numb"
+							name="numb"
+							className="mx-2 mt-1 block w-20 rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
+							defaultValue="10"
+						>
+							<option>10</option>
+							<option>25</option>
+							<option>50</option>
+							<option>100</option>
+						</select>
+						<label
+							htmlFor="location"
+							className="block text-sm font-medium text-gray-700 mt-3"
+						>
+							Entries
+						</label>
+					</div>
+					<div className="mt-4 sm:mt-0 sm:ml-16 sm:flex h-auto sm:w-1/4">
+						<label
+							htmlFor="firstname"
+							className="block text-sm font-medium text-gray-700 sm:mt-3 sm:mr-4"
+						>
+							Search
+						</label>
+						<div className="mt-1">
+							<input
+								type="text"
+								name="firstname"
+								id="firstname"
+								className="block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
+								placeholder="Search an employee"
+							/>
+						</div>
+						<Link
+							to="/create-employee"
+							className="sm:w-1/4 inline-flex items-center rounded-md border border-transparent bg-green-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+						>
+							Create an Employee
+						</Link>
+					</div>
+				</div>
+				<div className="mt-8 flex flex-col">
+					<div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+						<div className="inline-block min-w-full py-2 align-middle">
+							<div className="overflow-hidden shadow-sm ring-1 ring-black ring-opacity-5">
+								<table className="min-w-full divide-y divide-gray-300">
+									<thead className="bg-gray-50">
+										<tr>
+											<th
+												scope="col"
+												className=" py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8"
+											>
+												<a href="/" className="group inline-flex">
+													Firstname
+													<span className=" ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
+														<ChevronUpDownIcon
+															className="h-5 w-5"
+															aria-hidden="true"
+														/>
+													</span>
+												</a>
+											</th>
+											<th
+												scope="col"
+												className=" px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+											>
+												<a href="/" className="group inline-flex">
+													Lastname
+													<span className=" ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
+														<ChevronUpDownIcon
+															className="h-5 w-5"
+															aria-hidden="true"
+														/>
+													</span>
+												</a>
+											</th>
+											<th
+												scope="col"
+												className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+											>
+												<a href="/" className="group inline-flex">
+													Start Date
+													<span className=" ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
+														<ChevronUpDownIcon
+															className="h-5 w-5"
+															aria-hidden="true"
+														/>
+													</span>
+												</a>
+											</th>
+											<th
+												scope="col"
+												className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+											>
+												<a href="/" className="group inline-flex">
+													Department
+													<span className=" ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
+														<ChevronUpDownIcon
+															className="h-5 w-5"
+															aria-hidden="true"
+														/>
+													</span>
+												</a>
+											</th>
+											<th
+												scope="col"
+												className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+											>
+												<a href="/" className="group inline-flex">
+													Date of Birth
+													<span className=" ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
+														<ChevronUpDownIcon
+															className="h-5 w-5"
+															aria-hidden="true"
+														/>
+													</span>
+												</a>
+											</th>
+											<th
+												scope="col"
+												className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+											>
+												<a href="/" className="group inline-flex">
+													Street
+													<span className=" ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
+														<ChevronUpDownIcon
+															className="h-5 w-5"
+															aria-hidden="true"
+														/>
+													</span>
+												</a>
+											</th>
+											<th
+												scope="col"
+												className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+											>
+												<a href="/" className="group inline-flex">
+													City
+													<span className=" ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
+														<ChevronUpDownIcon
+															className="h-5 w-5"
+															aria-hidden="true"
+														/>
+													</span>
+												</a>
+											</th>
+											<th
+												scope="col"
+												className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+											>
+												<a href="/" className="group inline-flex">
+													State
+													<span className=" ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
+														<ChevronUpDownIcon
+															className="h-5 w-5"
+															aria-hidden="true"
+														/>
+													</span>
+												</a>
+											</th>
+											<th
+												scope="col"
+												className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+											>
+												<a href="/" className="group inline-flex">
+													Zip Code
+													<span className=" ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
+														<ChevronUpDownIcon
+															className="h-5 w-5"
+															aria-hidden="true"
+														/>
+													</span>
+												</a>
+											</th>
+											<th
+												scope="col"
+												className="relative py-3.5 pl-3 pr-4 sm:pr-6 lg:pr-8"
+											>
+												<span className="sr-only">Edit</span>
+											</th>
+										</tr>
+									</thead>
+									<tbody className="divide-y divide-gray-200 bg-white">
+										{people.map((person) => (
+											<tr key={person.email}>
+												<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
+													{person.Firstname}
+												</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+													{person.Lastname}
+												</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+													{person.StartDate}
+												</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+													{person.Department}
+												</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+													{person.BirthDate}
+												</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+													{person.Street}
+												</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+													{person.City}
+												</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+													{person.State}
+												</td>
+												<td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+													{person.Zipcode}
+												</td>
+												<td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 lg:pr-8">
+													<a
+														href="/"
+														className="text-green-600 hover:text-green-900"
+													>
+														Edit<span className="sr-only">, {person.name}</span>
+													</a>
+												</td>
+											</tr>
+										))}
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
