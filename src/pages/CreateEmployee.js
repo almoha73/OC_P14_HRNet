070c6957b1d547299e801ef10069db5567a1af0a
Agnes <almoha@gmail.com>
2022-12-13 15:00:12 +0100

    console data capture form

--- src/pages/CreateEmployee.js
+++ src/pages/CreateEmployee.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useForm, Controller } from "react-hook-form";
 import logo from "../assets/logo.jpg";
 import { Link } from "react-router-dom";
 import { states } from "../utils/States";
@@ -13,7 +16,10 @@ function classNames(...classes) {
 
 const CreateEmployee = () => {
 	const [selected, setSelected] = useState(states[0]);
-	const [selectedDpt, setSelectedDpt] = useState(department[3]);
+	const [selectedDpt, setSelectedDpt] = useState(department[0]);
+
+	const { control, register, handleSubmit } = useForm();
+	const onSubmit = (data) => console.log(data);
 
 	return (
 		<>
@@ -31,7 +37,10 @@ const CreateEmployee = () => {
 				Create Employee
 			</h2>
 
-			<form className="w-3/5 sm:w-2/5 mx-auto mt-8">
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className="w-3/5 sm:w-2/5 mx-auto mt-8"
+			>
 				<div>
 					<label
 						htmlFor="firstname"
@@ -41,6 +50,7 @@ const CreateEmployee = () => {
 					</label>
 
 					<input
+						{...register("firstname")}
 						type="text"
 						name="firstname"
 						id="firstname"
@@ -56,6 +66,7 @@ const CreateEmployee = () => {
 						Lastname
 					</label>
 					<input
+						{...register("lastname")}
 						type="text"
 						name="lastname"
 						id="lastname"
@@ -70,13 +81,17 @@ const CreateEmployee = () => {
 					>
 						Date of Birth
 					</label>
-
-					<input
-						type="date"
+					<Controller
 						name="birthDate"
-						id="birthDate"
-						className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
-						placeholder="birthDate"
+						control={control}
+						render={({ field }) => (
+							<DatePicker
+								placeholderText="Select the date of birth"
+								selected={field.value}
+								onChange={(date) => field.onChange(date)}
+								className="mt-1 w-full rounded border-gray-300 focus:border-green-500 focus:ring-green-500"
+							/>
+						)}
 					/>
 				</div>
 				<div className="mt-8">
@@ -87,12 +102,17 @@ const CreateEmployee = () => {
 						Start Date
 					</label>
 
-					<input
-						type="date"
-						name="startdate"
-						id="startdate"
-						className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-green-500 focus:ring-green-500 sm:text-sm"
-						placeholder="startdate"
+					<Controller
+						name="startDate"
+						control={control}
+						render={({ field }) => (
+							<DatePicker
+								placeholderText="Select the start date"
+								selected={field.value}
+								onChange={(date) => field.onChange(date)}
+								className="mt-1 w-full rounded border-gray-300 focus:border-green-500 focus:ring-green-500"
+							/>
+						)}
 					/>
 				</div>
 				<div className="adress border mt-8 bg-green-200">
@@ -106,6 +126,7 @@ const CreateEmployee = () => {
 						</label>
 
 						<input
+							{...register("street")}
 							type="text"
 							name="street"
 							id="street"
@@ -122,6 +143,7 @@ const CreateEmployee = () => {
 						</label>
 
 						<input
+							{...register("city")}
 							type="text"
 							name="city"
 							id="city"
@@ -129,81 +151,100 @@ const CreateEmployee = () => {
 							placeholder="city"
 						/>
 					</div>
-					<Listbox value={selected} onChange={setSelected}>
-						{({ open }) => (
-							<div className="w-1/2 mx-auto">
-								<Listbox.Label className="block text-sm font-medium text-gray-700 mt-8">
-									State
-								</Listbox.Label>
-								<div className="relative mt-1">
-									<Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-green-500 focus:outline-none focus:ring-1 focus:ring-green-500 sm:text-sm">
-										{states && (
-											<span className="block truncate">{selected.name}</span>
-										)}
+					<Controller
+						name="state"
+						control={control}
+						defaultValue={selected.name}
+						render={({ field: { onChange } }) => (
+							<Listbox
+								value={selected.name}
+								onChange={(e) => {
+									onChange(e);
+									setSelected(e);
+								}}
+							>
+								{({ open }) => (
+									<div className="w-1/2 mx-auto">
+										<Listbox.Label className="block text-sm font-medium text-gray-700 mt-8">
+											State
+										</Listbox.Label>
+										<div className="relative mt-1">
+											<Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-green-500 focus:outline-none focus:ring-1 focus:ring-green-500 sm:text-sm">
+												{states && (
+													<span className="block truncate">
+														{selected.name}
+													</span>
+												)}
 
-										<span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-											<ChevronUpDownIcon
-												className="h-5 w-5 text-gray-400"
-												aria-hidden="true"
-											/>
-										</span>
-									</Listbox.Button>
+												<span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+													<ChevronUpDownIcon
+														className="h-5 w-5 text-gray-400"
+														aria-hidden="true"
+													/>
+												</span>
+											</Listbox.Button>
 
-									<Transition
-										show={open}
-										as={Fragment}
-										leave="transition ease-in duration-100"
-										leaveFrom="opacity-100"
-										leaveTo="opacity-0"
-									>
-										<Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-											{states.map((state) => (
-												<Listbox.Option
-													key={state.id}
-													className={({ active }) =>
-														classNames(
-															active
-																? "text-white bg-green-600"
-																: "text-gray-900",
-															"relative cursor-default select-none py-2 pl-8 pr-4"
-														)
-													}
-													value={state}
-												>
-													{({ selected, active }) => (
-														<>
-															<span
-																className={classNames(
-																	selected ? "font-semibold" : "font-normal",
-																	"block truncate"
-																)}
-															>
-																{state.name}
-															</span>
+											<Transition
+												show={open}
+												as={Fragment}
+												leave="transition ease-in duration-100"
+												leaveFrom="opacity-100"
+												leaveTo="opacity-0"
+											>
+												<Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+													{states.map((state) => (
+														<Listbox.Option
+															key={state.id}
+															className={({ active }) =>
+																classNames(
+																	active
+																		? "text-white bg-green-600"
+																		: "text-gray-900",
+																	"relative cursor-default select-none py-2 pl-8 pr-4"
+																)
+															}
+															value={state}
+														>
+															{({ selected, active }) => (
+																<>
+																	<span
+																		className={classNames(
+																			selected
+																				? "font-semibold"
+																				: "font-normal",
+																			"block truncate"
+																		)}
+																	>
+																		{state.name}
+																	</span>
 
-															{selected ? (
-																<span
-																	className={classNames(
-																		active ? "text-white" : "text-green-600",
-																		"absolute inset-y-0 left-0 flex items-center pl-1.5"
-																	)}
-																>
-																	<CheckIcon
-																		className="h-5 w-5"
-																		aria-hidden="true"
-																	/>
-																</span>
-															) : null}
-														</>
-													)}
-												</Listbox.Option>
-											))}
-										</Listbox.Options>
-									</Transition>
-								</div>
-							</div>
+																	{selected ? (
+																		<span
+																			className={classNames(
+																				active
+																					? "text-white"
+																					: "text-green-600",
+																				"absolute inset-y-0 left-0 flex items-center pl-1.5"
+																			)}
+																		>
+																			<CheckIcon
+																				className="h-5 w-5"
+																				aria-hidden="true"
+																			/>
+																		</span>
+																	) : null}
+																</>
+															)}
+														</Listbox.Option>
+													))}
+												</Listbox.Options>
+											</Transition>
+										</div>
+									</div>
+								)}
+							</Listbox>
 						)}
-					</Listbox>
+					/>
 					<div className="mt-8 w-1/2 mx-auto mb-8">
 						<label
 							htmlFor="zip"
@@ -213,6 +254,7 @@ const CreateEmployee = () => {
 						</label>
 
 						<input
+							{...register("zip")}
 							type="number"
 							name="zip"
 							id="zip"
@@ -221,84 +263,101 @@ const CreateEmployee = () => {
 						/>
 					</div>
 				</div>
-				<Listbox value={selectedDpt} onChange={setSelectedDpt}>
-					{({ open }) => (
-						<div className="w-full mt-8">
-							<Listbox.Label className="block text-sm font-medium text-gray-700">
-								Department
-							</Listbox.Label>
-							<div className="relative mt-1">
-								<Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-green-500 focus:outline-none focus:ring-1 focus:ring-green-500 sm:text-sm">
-									{department && (
-										<span className="block truncate">{selectedDpt.name}</span>
-									)}
+				<Controller
+					name="department"
+					control={control}
+					defaultValue={selectedDpt.name}
+					render={({ field: { onChange } }) => (
+						<Listbox
+							value={selectedDpt}
+							onChange={(e) => {
+								onChange(e);
+								setSelectedDpt(e);
+							}}
+						>
+							{({ open }) => (
+								<div className="w-full mt-8">
+									<Listbox.Label className="block text-sm font-medium text-gray-700">
+										Department
+									</Listbox.Label>
+									<div className="relative mt-1">
+										<Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-green-500 focus:outline-none focus:ring-1 focus:ring-green-500 sm:text-sm">
+											{department && (
+												<span className="block truncate">
+													{selectedDpt.name}
+												</span>
+											)}
 
-									<span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-										<ChevronUpDownIcon
-											className="h-5 w-5 text-gray-400"
-											aria-hidden="true"
-										/>
-									</span>
-								</Listbox.Button>
+											<span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+												<ChevronUpDownIcon
+													className="h-5 w-5 text-gray-400"
+													aria-hidden="true"
+												/>
+											</span>
+										</Listbox.Button>
 
-								<Transition
-									show={open}
-									as={Fragment}
-									leave="transition ease-in duration-100"
-									leaveFrom="opacity-100"
-									leaveTo="opacity-0"
-								>
-									<Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-										{department.map((elt) => (
-											<Listbox.Option
-												key={elt.id}
-												className={({ active }) =>
-													classNames(
-														active
-															? "text-white bg-green-600"
-															: "text-gray-900",
-														"relative cursor-default select-none py-2 pl-3 pr-9"
-													)
-												}
-												value={elt}
-											>
-												{({ selectedDpt, active }) => (
-													<>
-														<span
-															className={classNames(
-																selectedDpt ? "font-semibold" : "font-normal",
-																"block truncate"
-															)}
-														>
-															{elt.name}
-														</span>
+										<Transition
+											show={open}
+											as={Fragment}
+											leave="transition ease-in duration-100"
+											leaveFrom="opacity-100"
+											leaveTo="opacity-0"
+										>
+											<Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+												{department.map((elt) => (
+													<Listbox.Option
+														key={elt.id}
+														className={({ active }) =>
+															classNames(
+																active
+																	? "text-white bg-green-600"
+																	: "text-gray-900",
+																"relative cursor-default select-none py-2 pl-3 pr-9"
+															)
+														}
+														value={elt}
+													>
+														{({ selectedDpt, active }) => (
+															<>
+																<span
+																	className={classNames(
+																		selectedDpt
+																			? "font-semibold"
+																			: "font-normal",
+																		"block truncate"
+																	)}
+																>
+																	{elt.name}
+																</span>
 
-														{selectedDpt ? (
-															<span
-																className={classNames(
-																	active ? "text-white" : "text-green-600",
-																	"absolute inset-y-0 right-0 flex items-center pr-4"
-																)}
-															>
-																<CheckIcon
-																	className="h-5 w-5"
-																	aria-hidden="true"
-																/>
-															</span>
-														) : null}
-													</>
-												)}
-											</Listbox.Option>
-										))}
-									</Listbox.Options>
-								</Transition>
-							</div>
-						</div>
+																{selectedDpt ? (
+																	<span
+																		className={classNames(
+																			active ? "text-white" : "text-green-600",
+																			"absolute inset-y-0 right-0 flex items-center pr-4"
+																		)}
+																	>
+																		<CheckIcon
+																			className="h-5 w-5"
+																			aria-hidden="true"
+																		/>
+																	</span>
+																) : null}
+															</>
+														)}
+													</Listbox.Option>
+												))}
+											</Listbox.Options>
+										</Transition>
+									</div>
+								</div>
+							)}
+						</Listbox>
 					)}
-				</Listbox>
+				/>
 				<div className="w-full flex justify-center mt-8 mb-8">
 					<button
-						type="button"
+						type="submit"
 						className="inline-flex items-center rounded-md border border-transparent bg-green-100 px-6 py-3 text-base font-medium text-green-700 hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
 					>
 						Save
