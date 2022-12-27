aadb2ec4203c13dd508e6fde6afb198e1f54e781
Agnes <almoha@gmail.com>
2022-12-27 18:51:31 +0100

    modification of the calendar

--- src/components/Form/utils/InputDate.js
+++ src/components/Form/utils/InputDate.js
@@ -3,19 +3,42 @@ import DatePicker from "react-datepicker";
 import { CalendarIcon } from "@heroicons/react/20/solid";
 
 const InputDate = (props) => {
-  const { text, value, fn } = props;
+	const { text, value, fn } = props;
 
-  return (
-    <>
-      <DatePicker
-        placeholderText={text}
-        selected={value}
-        onChange={fn}
-        className=" mt-1 w-full rounded border-gray-300 focus:border-green-500 focus:ring-green-500 placeholder:text-xs sm:placeholder:text-sm"
-      />
-      <CalendarIcon className="w-5 relative left-[90%] bottom-8" />
-    </>
-  );
+	return (
+		<>
+			<DatePicker
+				placeholderText={text}
+				selected={value}
+				onChange={fn}
+				dateFormat="dd/MM/yyyy"
+				peekNextMonth
+				showMonthDropdown
+				showYearDropdown
+				dropdownMode="select"
+				popperClassName="some-custom-class"
+				popperPlacement="bottom-start"
+				popperModifiers={[
+					{
+						name: "offset",
+						options: {
+							offset: [5, 10],
+						},
+					},
+					{
+						name: "preventOverflow",
+						options: {
+							rootBoundary: "viewport",
+							tether: false,
+							altAxis: true,
+						},
+					},
+				]}
+				className=" mt-1 w-full rounded border-gray-300 focus:border-green-500 focus:ring-green-500 placeholder:text-xs sm:placeholder:text-sm"
+			/>
+			<CalendarIcon className="w-5 relative left-[90%] bottom-8" />
+		</>
+	);
 };
 
 export default InputDate;
