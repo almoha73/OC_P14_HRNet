865e50a9b4911c41fdf43f77becfcc72e5d67637
Agnes <almoha@gmail.com>
2023-01-12 09:25:18 +0100

    post pull

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,11 +7,16 @@ import { useForm } from "react-hook-form";
 export default function Login() {
   const { signIn } = useContext(AuthContext);
   const navigate = useNavigate();
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = async (data) => {
+    console.log(data);
     try {
-      await signIn(data.email, data.password);
+      await signIn(data?.email, data?.password)
       navigate("/private/home");
     } catch (error) {
       console.log(error);
@@ -36,56 +41,54 @@ export default function Login() {
           <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
             <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
               <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Admin Email address
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      {...register("email")}
-                      placeholder="Your admin email"
-                      id="email"
-                      name="email"
-                      type="email"
-                      // autoComplete="email"
-                      required
-                      className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
-                    />
-                  </div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Admin Email address
+                </label>
+                <div className="mt-1">
+                  <input
+                    {...register("email", { required: true })}
+                    placeholder="Your admin email"
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
+                  />
+                  {errors.email && (
+                    <span style={{ color: "red" }}>*Email* is mandatory </span>
+                  )}
                 </div>
 
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Admin Password
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      {...register("password")}
-                      placeholder="Your admin password"
-                      id="password"
-                      name="password"
-                      type="password"
-                      // autoComplete="current-password"
-                      required
-                      className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
-                    />
-                  </div>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Admin Password
+                </label>
+                <div className="mt-1">
+                  <input
+                    {...register("password", { required: true })}
+                    placeholder="Your admin password"
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
+                  />
+                  {errors.password && (
+                    <span style={{ color: "red" }}>
+                      *Password* is mandatory{" "}
+                    </span>
+                  )}
                 </div>
 
-                <div>
-                  <button
-                    type="submit"
-                    className="flex w-full justify-center rounded-md border border-transparent bg-green-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-green-200 hover:text-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
-                  >
-                    Sign in
-                  </button>
-                </div>
+                <input
+                  type="submit"
+                  className="flex w-full justify-center rounded-md border border-transparent bg-green-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-green-200 hover:text-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+                />
               </form>
             </div>
             <p className="text-green-700 text-xs text-center mt-12">
