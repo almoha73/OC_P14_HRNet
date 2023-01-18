e65a8de5b7f80a114e68dc1461dff4f814a44061
Agnes <almoha@gmail.com>
2023-01-18 17:26:45 +0100

    ...

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -16,7 +16,7 @@ export default function Login() {
 	const onSubmit = async (data) => {
     console.log(data);
 		try {
-			await signIn(data??.email, data??.password)
+			await signIn(data?.email, data?.password)
 			navigate("/private/home");
 		} catch (error) {
 			console.log(error);
@@ -38,29 +38,7 @@ export default function Login() {
 						</h2>
 					</div>
 
-					<div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-						<div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-							<form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-								<div>
-									<label
-										htmlFor="email"
-										className="block text-sm font-medium text-gray-700"
-									>
-										Admin Email address
-									</label>
-									<div className="mt-1">
-										<input
-											{...register("email")}
-											placeholder="Your admin email"
-											id="email"
-											name="email"
-											type="email"
-											// autoComplete="email"
-											required
-											className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm"
-										/>
-									</div>
-								</div>
+					
           <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
             <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
               <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
